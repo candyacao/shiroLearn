3d8de66f3e5c65a9dcde7f94a58a4cefdc2e93a7
caoyajuan <caoyajuan@dhgate.com>
2021-02-26 18:24:10 +0800

    update

--- demo/src/main/java/com/example/nonetty/PlainNIOServer.java
+++ demo/src/main/java/com/example/nonetty/PlainNIOServer.java
@@ -1,8 +1,81 @@
-package com.example.nonetty;/**
-  * @ClassName PlainNIOServer
-  * @Description TODO
-  * @author caoyajuan
-  * @date 2021/2/26 17:58
-*/
+package com.example.nonetty;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author caoyajuan
+ * @ClassName PlainNIOServer
+ * @Description TODO
+ * @date 2021/2/26 17:58
+ */
 public class PlainNIOServer {
+    public void server(int port) throws Exception{
+        ServerSocketChannel socketChannel = ServerSocketChannel.open();
+        socketChannel.configureBlocking(false);
+        ServerSocket socket = socketChannel.socket();
+        InetSocketAddress address = new InetSocketAddress(port);
+        //将服务器绑定到选定的端口
+        socket.bind(address);
+        // 打开Selector来处理Channel
+        Selector selector = Selector.open();
+        // 将ServerSocket 注册到Selector以接受连接
+        socketChannel.register(selector, SelectionKey.OP_ACCEPT);
+        final ByteBuffer msg = ByteBuffer.wrap("Hi!\r\n".getBytes());
+        for (;;){
+            try {
+                selector.select();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                // handle exception
+                break;
+            }
+            Set<SelectionKey> readyKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = readyKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                iterator.remove();
+                try {
+                    if (key.isAcceptable()) {
+                        ServerSocketChannel server =
+                                (ServerSocketChannel) key.channel();
+                        SocketChannel client = server.accept();
+                        client.configureBlocking(false);
+                        client.register(selector, SelectionKey.OP_WRITE |
+                                SelectionKey.OP_READ, msg.duplicate());
+                        System.out.println(
+                                "Accepted connection from " + client);
+                    }
+                    if (key.isWritable()) {
+                        SocketChannel client =
+                                (SocketChannel) key.channel();
+                        ByteBuffer buffer =
+                                (ByteBuffer) key.attachment();
+                        while (buffer.hasRemaining()) {
+                            if (client.write(buffer) == 0) {
+                                break;
+                            }
+                        }
+                        client.close();
+                    }
+                } catch (IOException ex) {
+                    key.cancel();
+                    try {
+                        key.channel().close();
+                    } catch (IOException cex) {
+                        // ignore on close
+                    }
+                }
+            }
+        }
+    }
 }
--- demo/src/main/java/com/example/nonetty/PlainOIOServer.java
+++ demo/src/main/java/com/example/nonetty/PlainOIOServer.java
@@ -1,8 +1,53 @@
-package com.example.nonetty;/**
-  * @ClassName PlainOIOServer
-  * @Description TODO
-  * @author caoyajuan
-  * @date 2021/2/26 17:48
-*/
+package com.example.nonetty;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+/**
+ * @author caoyajuan
+ * @ClassName PlainOIOServer
+ * @Description TODO
+ * @date 2021/2/26 17:48
+ */
 public class PlainOIOServer {
+    public void server(int port) throws Exception{
+        // 将服务器绑定到指定端口
+        final ServerSocket serverSocket = new ServerSocket(port);
+        try{
+            for (;;){
+                // 接受连接
+                final Socket clientSocket = serverSocket.accept();
+                System.out.println("Acceptted connection from " + clientSocket);
+                // 创建一个新线程来处理该连接
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        OutputStream os;
+                        try{
+                            os = clientSocket.getOutputStream();
+                            // 将消息写给已连接的客户端
+                            os.write("Hi!\r\n".getBytes(
+                                    Charset.forName("UTF-8")));
+                            os.flush();
+                            clientSocket.close();
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            try{
+                                clientSocket.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start(); // 启动线程
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
